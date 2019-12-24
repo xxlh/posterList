0e46760636a0b0c43935d6a49dc2058ce20c5633
xxlh <0@eexx.me>
2019-12-24 15:54:56 +0800

    item getHeight

--- app/js/WaterfallSampleComponent.js
+++ app/js/WaterfallSampleComponent.js
@@ -4,8 +4,16 @@ import ReactDOM from 'react-dom';
 import AutoResponsive from 'autoresponsive-react';
 import axios from "axios"
 import qs from 'qs';
+import { PullToRefresh } from 'antd-mobile';
 
 const pageSize = 10;
+let styleList=[];
+let getItemStyle = function() {
+	return {
+	  width: 150,
+	  height: 200,
+	};
+  };
 class WaterfallSampleComponent extends React.Component {
 	constructor(porps){
 		super(porps);
@@ -16,6 +24,7 @@ class WaterfallSampleComponent extends React.Component {
 			hotKeyword : "" ,
 			page: 1,
 			article: [],//文章列表
+			styleList : [],//图片样式主要获取高度
 			total:0,
 			isLoading: true,
 			refreshing: true,
@@ -32,15 +41,30 @@ class WaterfallSampleComponent extends React.Component {
 		data.search=searchWord;
 		axios.post('https://sina.ieexx.com/api/public/?s=Projects.getList',qs.stringify(data)).then(function(response){
 			if(response.data.ret == 200 ){
+				 response.data.data.items.map((i,index) => {
+					styleList[index] = getItemStyle();
+					let img = new Image();
+					img.src = i.imgurl;
+					img.onload= () => {
+						styleList[index].height = img.height / img.width * styleList[index].width;
+						self.setState({
+							styleList: styleList
+						  });
+					}
+				});
 				if (response.data.data.page == 1) {//如果是第一页，直接覆盖之前的数据
 					self.setState({
-						article:[...response.data.data.items]
+						article:[...response.data.data.items],
+						styleList :styleList
 					});
 				} else {
 					self.setState({
 						article:[...self.state.article, ...response.data.data.items],
+						styleList :styleList
 					});
 				}
+				// console.log(styleList);
+				// getItemStyle();
 				// if(response.data.data.total <= page * pageSize){}
 			}else{
 				swal(response.data.msg);
@@ -49,6 +73,7 @@ class WaterfallSampleComponent extends React.Component {
 		})
         
 	}
+	
 	componentDidMount() {
 		this.getData(1, this.state.hotKeyword);
 	  }
@@ -59,7 +84,8 @@ class WaterfallSampleComponent extends React.Component {
 		  containerHeight : this.state.height,
 		  itemClassName: 'item',
 		  gridWidth: 100,
-		  transitionDuration: '.5'
+		  transitionDuration: '.8',
+		  transitionTimingFunction: 'easeIn'
 		};
 	  }
 	render() {
@@ -67,13 +93,10 @@ class WaterfallSampleComponent extends React.Component {
 		<div className="albumPanel">
 			<AutoResponsive ref="container" {...this.getAutoResponsiveProps()}>
 			{
-				this.state.article.map(function(i,index) {
-					let style = {
-						width: '3rem',
-						height: 200
-					};
+				this.state.article.map((i,index) => {
+					console.log(this.state.styleList[index])
 					return (
-						<div key={index}  className={`w1 album item`} style={style}>
+						<div key={index}  className={`w1 album item`} style={this.state.styleList[index]}>
 						<img className="a-cover" src={i.imgurl}/>
 						<p className="a-layer">
 							<span className="al-title">{i.title}</span>
@@ -82,6 +105,7 @@ class WaterfallSampleComponent extends React.Component {
 					);
 					})
 			}
+			
 			</AutoResponsive>
 		</div>
 	  );
