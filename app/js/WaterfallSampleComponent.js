95fa0bd0b06208eb2eeb065045e76ca399605539
xxlh <0@eexx.me>
2020-01-14 14:25:18 +0800

    search

--- app/js/WaterfallSampleComponent.js
+++ app/js/WaterfallSampleComponent.js
@@ -11,8 +11,9 @@ import 'antd/dist/antd.css';
 import locale from 'antd/es/date-picker/locale/zh_CN';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
-import { Select, Input, DatePicker } from 'antd';
+import { Drawer, Button, Select, Input, DatePicker } from 'antd';
 import { CacheLink  } from "react-keeper";
+
 
 moment.locale('zh-cn');
 const { Search } = Input;
@@ -45,18 +46,36 @@ class WaterfallSampleComponent extends React.Component {
 			typelaber: [],
 			startTime:undefined,//开始时间
 			endTime:undefined,  //结束时间
-			timeRang:[]
+			timeRang:[],
+			startValue: null,
+			endValue: null,
+			endOpen: false,
+			visible: false,
 		};
 	}
 
-	// search 
-	onChange= (value) => {
-		this.setState({ 
-			searchkeyword: {...this.state.searchkeyword, name: value},
-			page : 1
-		} ,()=>{
-			this.getData(this.state.page, this.state.searchkeyword);
+	showDrawer = () => {
+		this.setState({
+		  visible: true,
 		});
+	  };
+	
+	  onClose = () => {
+		this.setState({
+		  visible: false,
+		});
+	  };
+
+	  
+	// search 
+	onNameChange = (e) => {
+		console.log(e.target.value)
+		this.setState({ 
+			searchkeyword: {...this.state.searchkeyword, name: e.target.value},
+			page : 1
+		} 
+		// ,()=>{this.getData(this.state.page, this.state.searchkeyword);}
+		);
 	};
 	
 	// 分类搜索
@@ -88,30 +107,84 @@ class WaterfallSampleComponent extends React.Component {
 			this.setState({ 
 				searchkeyword: {...this.state.searchkeyword, sceneids: value.join(",")},
 				page : 1
-			} ,()=>{
-				this.getData(this.state.page, this.state.searchkeyword);
-			});
+			} 
+			// ,()=>{this.getData(this.state.page, this.state.searchkeyword);}
+				);
 		}else{
 			this.setState({ 
 				searchkeyword: {...this.state.searchkeyword, typeids: value.join(",")},
 				page : 1
-			} ,()=>{
-				this.getData(this.state.page, this.state.searchkeyword);
-			});
+			}
+			// ,()=>{this.getData(this.state.page, this.state.searchkeyword);}
+			);
 		}
 	  }
 
-	  handleTimeChange = (date, dateString) => {
-		console.log(dateString);
-		if(dateString!= undefined )
+	handleTimeChange = (date, dateString) => {
+	console.log(dateString);
+	if(dateString!= undefined )
+	this.setState({
+		searchkeyword: {...this.state.searchkeyword, startdate: dateString[0] ,enddate:  dateString[1]},
+		page : 1
+	} 
+	// ,()=>{this.getData(this.state.page, this.state.searchkeyword);}
+	);
+	
+	}
+
+	disabledStartDate = startValue => {
+	const { endValue } = this.state;
+	if (!startValue || !endValue) {
+		return false;
+	}
+	return startValue.valueOf() > endValue.valueOf();
+	};
+
+	disabledEndDate = endValue => {
+	const { startValue } = this.state;
+	if (!endValue || !startValue) {
+		return false;
+	}
+	return endValue.valueOf() <= startValue.valueOf();
+	};
+
+
+	onStartChange = (date, dateString) => {
 		this.setState({
-			searchkeyword: {...this.state.searchkeyword, startdate: dateString[0] ,enddate:  dateString[1]},
-			page : 1
-		} ,()=>{
-			this.getData(this.state.page, this.state.searchkeyword);
-		});
-		
-	  }
+			page : 1,
+			searchkeyword: {...this.state.searchkeyword, startdate: dateString },
+			startValue :date,
+		},
+		// () =>{this.getData(this.state.page, this.state.searchkeyword);}
+		);
+	};
+
+	onEndChange = (date, dateString) => {
+		this.setState({
+			page : 1,
+			searchkeyword: {...this.state.searchkeyword, enddate: dateString },
+			endValue :date,
+		},
+		// () =>{this.getData(this.state.page, this.state.searchkeyword);}
+		);
+	};
+
+	submit = () =>{
+		this.setState({
+			visible: false,
+		  },
+		  () =>{this.getData(this.state.page, this.state.searchkeyword);}
+		  );
+	}
+	//   handleStartOpenChange = open => {
+	// 	if (!open) {
+	// 	  this.setState({ endOpen: true });
+	// 	}
+	//   };
+	
+	//   handleEndOpenChange = open => {
+	// 	this.setState({ endOpen: false });
+	//   };
 
 	// 列表数据
 	getData(page, searchWord) {//获取数据的函数
@@ -129,7 +202,7 @@ class WaterfallSampleComponent extends React.Component {
 						styleList.push(getItemStyle());
 						self.setState({styleList});
 						let img = new Image();
-						img.src = v.imgurl;
+						img.src = v.coverimg;
 						img.index = styleList.length - 1;
 						img.onload= () => {
 							let styleList = self.state.styleList;
@@ -203,6 +276,7 @@ class WaterfallSampleComponent extends React.Component {
 			itemMargin: 10,
 			containerWidth: this.state.containerWidth || document.body.clientWidth,
 			itemClassName: 'item',
+			containerHeight : this.state.height,
 			gridWidth: 100,
 			transitionDuration: '.5'
 		  
@@ -212,45 +286,84 @@ class WaterfallSampleComponent extends React.Component {
 	  
 
 	render() {
+		const { startValue, endValue } = this.state;
 	  return (
 		<section className="page">
-			<Select
-				mode="multiple"
-				style={{ width: '30%', margin:'10px'}}
-				placeholder="海报场景(来源)"
-				defaultValue={[]}
-				allowClear
-				showArrow
-				onChange={v => this.handleChange(1, v)}
+			<Button type="primary" onClick={this.showDrawer} style={{position:'absolute', top:'0', right:'5%',}}>
+				搜索
+			</Button>
+			<Drawer
+				title="搜索条件"
+				placement="left"
+				closable={false}
+				onClose={this.onClose}
+				visible={this.state.visible}
+				maskClosable={this.state.visible}
 			>
-				{this.state.comeFrom}
-			</Select>
-			<Select
-				mode="multiple"
-				style={{ width: '30%', margin:'10px' }}
-				placeholder="海报类别"
-				defaultValue={[]}
-				allowClear
-				showArrow
-				onChange={v => this.handleChange(2, v)}
-			>
+				<DatePicker
+					style={{ width: '100%', margin:'5px'}}
+					disabledDate={this.disabledStartDate}
+					// showTime
+					format= {dateFormat}
+					value={startValue}
+					placeholder="开始时间"
+					onChange={this.onStartChange}
+					// onOpenChange={this.handleStartOpenChange}
+				/>
+				<DatePicker
+					style={{ width: '100%', margin:'5px'}}
+					disabledDate={this.disabledEndDate}
+					// showTime
+					format= {dateFormat}
+					value={endValue}
+					placeholder="结束时间"
+					onChange={this.onEndChange}
+					// open={endOpen}
+					// onOpenChange={this.handleEndOpenChange}
+				/>
+				<Select
+					mode="multiple"
+					style={{ width: '100%', margin:'5px'}}
+					placeholder="海报场景(来源)"
+					defaultValue={[]}
+					allowClear
+					showArrow
+					onChange={v => this.handleChange(1, v)}
+				>
+					{this.state.comeFrom}
+				</Select>
+				<Select
+					mode="multiple"
+					style={{ width: '100%', margin:'5px' }}
+					placeholder="海报类别"
+					defaultValue={[]}
+					allowClear
+					showArrow
+					onChange={v => this.handleChange(2, v)}
+				>
 					{this.state.typelaber}
-			</Select>
-			<RangePicker locale={locale} showTime 
+				</Select>
+				<Input style={{ width: '100%', margin:'5px'}} allowClear  placeholder="名 字" onChange={this.onNameChange} />
+				<Button type="primary" onClick={this.submit} style={{}}>
+				  查询
+				</Button>
+				{/* <Search
+					style={{ width: '100%', margin:'5px'}}
+					placeholder="input search text"
+					enterButton="Search"
+					size="default"
+					style={{ width: '40%'}}
+					onSearch={(value) => this.onChange(value)}
+				/> */}
+			</Drawer>
+			
+			{/* <RangePicker style={{ width: '40%', margin:'10px' }} locale={locale} showTime 
 				// renderExtraFooter={() => 'extra footer'}
 				format = {dateFormat}
 				// onChange = {this.handleTimeChange}
 				// value={this.state.timeRang}
 				onOk = {this.handleTimeChange}
-			/>
-			<Search
-				placeholder="input search text"
-				enterButton="Search"
-				size="default"
-				style={{ width: '40%'}}
-				onSearch={(value) => this.onChange(value)}
-			/>
-			
+			/> */}
 			<section className="stage" ref="stage">
 				<section className="img-sec">
 					<div className="albumPanel" id="scrollableDiv" style={{width: this.state.containerWidth, height: this.state.height, overflow: "auto" }} >
@@ -258,12 +371,12 @@ class WaterfallSampleComponent extends React.Component {
 					{
 						this.state.article.map((v,i) => {
 							return (
-								<div  to='/detial'  state ={{gid:v.gid}} key={i}  className={`w${i} album item`} style={this.state.styleList[i]}   >
+								<CacheLink  to='/detial'  state ={{gid:v.gid}} key={i}  className={`w${i} album item`} style={this.state.styleList[i]}   >
 								<img className="a-cover" src={v.coverimg}/>
 								<p className="a-layer">
 									<span className="al-title">{v.name}</span>
 								</p>
-								</div>
+								</CacheLink>
 							);
 						})
 					}
@@ -272,7 +385,7 @@ class WaterfallSampleComponent extends React.Component {
 						dataLength={this.state.article}
 						next={this.onEndReached}
 						hasMore={true}
-						loader={<h4> {this.state.isLoading ? "我是有底线的~" : "加载中..."}</h4>}
+						// loader={<h4> {this.state.isLoading ? "我是有底线的~" : "加载中..."}</h4>}
 						scrollableTarget="scrollableDiv"
 					>
 					</InfiniteScroll>
